66ed63d3b26d81705292c41b716fa54aeb84c60f
harislapo <haris.lapo@gmail.com>
2023-09-04 20:40:42 +0200

    Added external news data file.

---
+++ dummyData.js
@@ -0,0 +1,39 @@
+const news = [
+  {
+    id: Math.floor(Math.floor(Math.random() * 3242578 + 1)),
+    title: 'Biden sworn in as the U.S. president.',
+    date: 'January 20, 2021',
+    text: 'Democrat Joe Biden was sworn in as the 46th president of the United States on Wednesday during a ceremony at a heavily protected U.S. Capitol, ending the tumultuous four-year presidency of his Republican predecessor Donald Trump.',
+  },
+  {
+    id: Math.floor(Math.floor(Math.random() * 3242578 + 1)),
+    title: 'Greenland permanently bans all oil and gas exploration.',
+    date: 'December 29, 2021',
+    text: 'In exciting news for the planet and environmentalists, Greenland has announced it is permanently halting all new oil and gas exploration in the country.',
+  },
+  {
+    id: Math.floor(Math.floor(Math.random() * 3242578 + 1)),
+    title: 'Boris Johnson Has Tested Positive For Coronavirus.',
+    date: 'March 27, 2020',
+    text: 'UK prime minister Boris Johnson has tested positive for the coronavirus after developing mild symptoms of COVID-19 and is now self-isolating in Downing Street.',
+  },
+  {
+    id: Math.floor(Math.floor(Math.random() * 3242578 + 1)),
+    title: 'Belgium says loot boxes are gambling, wants them banned in Europe.',
+    date: 'November 22, 2017',
+    text: "Belgium's Gaming Commission announced that it had launched an investigation into whether the loot boxes available for purchase in games like Overwatch and Star Wars Battlefront 2 constitute a form of gambling. ",
+  },
+  {
+    id: Math.floor(Math.floor(Math.random() * 3242578 + 1)),
+    title:
+      'Giraffes just silently went to the list of endangered animals facing extinction',
+    date: 'December 15, 2018',
+    text: 'Two subspecies of giraffes were recently added to the list of "critically endangered" species for the first time ever, as per athelatest report by the (IUCN), which administers the world\'s official endangered species list.',
+  },
+  {
+    id: Math.floor(Math.floor(Math.random() * 3242578 + 1)),
+    title: 'Queen Elizabeth II has died, Buckingham Palace announces.',
+    date: 'September 8, 2022',
+    text: "Queen Elizabeth II, the UK's longest-serving monarch, has died at Balmoral aged 96, after reigning for 70 years.",
+  },
+];
