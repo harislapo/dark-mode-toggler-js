7041b62fdecd1f75131feb3715d052269944ec8a
harislapo <haris.lapo@gmail.com>
2023-09-05 16:04:16 +0200

    Added button logic and variables in css to ease up the mode toggling logic.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const newsFeedContainer = document.querySelector('.news-feed');
+const toggleBtn = document.querySelector('.btn');
+
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+});
 
 const newsFeedData = news
   // Loop through each item.
   .map((item) => {
     // Destructure object keys that we need.
     const { title, date, text } = item;
+    // Return an article DOM element filled with data from external file.
     return `<article class="news-feed-item">
   <h2>${title}</h2>
   <div class="news-info">
